33fb39078c056591eeb03e13fa738a45ad146799
Jens Bissinger <mail@jens-bissinger.de>
2014-10-13 10:14:23 +0200

    Cleanup app.go.

--- app.go
+++ app.go
@@ -15,24 +15,22 @@ func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 
-type Controller struct{}
-
-func (this Controller) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	headers := w.Header()
 	headers.Add("Content-Type", "text/html")
 	io.WriteString(w, "<html><head></head><body><p>Hello world from Go!</p></body></html>")
 }
 
 func main() {
-	controller := Controller{}
+	http.HandleFunc("/", ServeHTTP)
 
 	flag.Parse()
 	var err error
 
 	if *local != "" { // Run as a local web server
-		err = http.ListenAndServe(*local, controller)
+		err = http.ListenAndServe(*local, nil)
 	} else { // Run as FCGI via standard I/O
-		err = fcgi.Serve(nil, controller)
+		err = fcgi.Serve(nil, nil)
 	}
 	if err != nil {
 		log.Fatal(err)
